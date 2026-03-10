50aaeba9bd3ed297974edffc98c80c899a76b4e9
shashikala-saradar <shashikalasardar2@gmail.com>
2026-03-10 08:06:27 +0530

    Delete Udemy projects/Calculator.py

--- Udemy projects/Calculator.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the tip calculator!")
-total_bill=float(input("What was the total bill?"))
-tips_a=int(input("How much tip would u like to give?10,12,or 15"))
-splitting=int(input("how many people to split the bill"))
-total_bill_to_be_payed=(total_bill+tips_a)/splitting
-print(f"Each person should pay {total_bill_to_be_payed:.3f}")
